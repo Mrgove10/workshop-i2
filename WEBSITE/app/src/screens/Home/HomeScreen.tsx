450d45874c7c829a74f5f3b75dde46ad0248d5a7
Edouard <edouardclisson@gmail.com>
2021-11-23 10:49:10 +0100

    Clean unused files and setup prject structure

---
+++ WEBSITE/app/src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function HomeScreen(props: any) {
+    return (
+        <div>
+            Home Page
+        </div>
+    )
+}
+
+HomeScreen.propTypes = {
+
+}
+
+export default HomeScreen
+
