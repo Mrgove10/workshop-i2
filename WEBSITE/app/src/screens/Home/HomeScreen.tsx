6266d4f094a36e3ae5e44d6532209f2951bc879d
Edouard <edouardclisson@gmail.com>
2021-11-23 13:10:47 +0100

    Add home page video iframe

--- WEBSITE/app/src/screens/Home/HomeScreen.tsx
+++ WEBSITE/app/src/screens/Home/HomeScreen.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './style.css'
 
 function HomeScreen(props: any) {
     return (
-        <div>
-            Home Page
+        <div className="container">
+            <div className="iframeContainer">
+                <iframe
+                    height="400"
+                    width="2"
+                    src="https://www.youtube.com/embed/G1IbRujko-A"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen>
+                </iframe>
+            </div>
         </div>
     )
 }
@@ -14,4 +25,3 @@ HomeScreen.propTypes = {
 }
 
 export default HomeScreen
-
