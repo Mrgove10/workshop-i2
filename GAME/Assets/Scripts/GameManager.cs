74ac4f666d209b8f13e95dcef45f215d45aa97d6
Romain CHAHINE <romain.chahine@rwigo.com>
2021-11-25 11:34:12 +0100

    update ui and add tooltips

--- GAME/Assets/Scripts/GameManager.cs
+++ GAME/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using NaughtyAttributes;
 using Statics;
 using UnityEngine;
@@ -14,25 +15,47 @@ public class GameManager : MonoBehaviour
     [ReadOnly]
     public float budget = 0;
 
+    // Bar
     public GameObject BudgetBar;
     public GameObject QuantiumBar;
     public GameObject SecurityBar;
 
+    // Popup
     public GameObject popup;
     public TMP_Text PopUpTitle;
     public TMP_Text PopUpDescription;
     public GameObject Action1;
     public GameObject Action2;
-
+    public TMP_Text Action1Libelle;
+    public TMP_Text Action2Libelle;
     public Button popupCloseButton;
+
+
+    // Tooltip
+    public GameObject Tooltip;
+    public Button TooltipAction1;
+    public Button TooltipAction2;
+    public TMP_Text TooltipTxt;
+    public string[] helpArray = new string[2] { "", "" };
+    public Button TooltipCloseButton;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+
         // in case we forgot to uncheck it in ht editor
         ClosePopup();
         // add a listening on the close button
         popupCloseButton.onClick.AddListener(ClosePopup);
+
+
+        Tooltip.SetActive(false);
+        TooltipCloseButton.onClick.AddListener(CloseTooltip);
+
+        TooltipAction1.onClick.AddListener(delegate { LaunchTooltip(0); });
+        TooltipAction2.onClick.AddListener(delegate { LaunchTooltip(1); });
     }
 
     // Update is called once per frame
@@ -53,6 +76,33 @@ public class GameManager : MonoBehaviour
         popup.SetActive(false);
     }
 
+    private void SetPopupActions(List<Actions> actionsList)
+    {
+        Action1Libelle.text = actionsList[0].Nom;
+        helpArray[0] = actionsList[0].Description;
+        try
+        {
+            Action2Libelle.text = actionsList[1].Nom;
+            helpArray[1] = actionsList[1].Description;
+            Action2.SetActive(true);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Action2.SetActive(false);
+        }
+    }
+
+    private void CloseTooltip()
+    {
+        Tooltip.SetActive(false);
+    }
+
+    private void LaunchTooltip(int index)
+    {
+        TooltipTxt.text = helpArray[index];
+        Tooltip.SetActive(true);
+    }
+
     public void LaunchEffect(AreaTypes type)
     {
 
@@ -61,38 +111,47 @@ public class GameManager : MonoBehaviour
             case AreaTypes.OnlinePayement:
                 PopUpTitle.text = OnlinePayementInfo.Nom;
                 PopUpDescription.text = OnlinePayementInfo.Description;
+                SetPopupActions(OnlinePayementInfo.ActionsList);
                 break;
             case AreaTypes.HrDepartment:
                 PopUpTitle.text = HrDepartmentInfo.Nom;
                 PopUpDescription.text = HrDepartmentInfo.Description;
+                SetPopupActions(HrDepartmentInfo.ActionsList);
                 break;
             case AreaTypes.ItDepartment:
                 PopUpTitle.text = ItDepartmentInfo.Nom;
                 PopUpDescription.text = ItDepartmentInfo.Description;
+                SetPopupActions(ItDepartmentInfo.ActionsList);
                 break;
             case AreaTypes.SalesDepartment:
                 PopUpTitle.text = SalesDepartmentInfo.Nom;
                 PopUpDescription.text = SalesDepartmentInfo.Description;
+                SetPopupActions(SalesDepartmentInfo.ActionsList);
                 break;
             case AreaTypes.DatabaseSecurity:
                 PopUpTitle.text = DatabaseSecurityInfo.Nom;
                 PopUpDescription.text = DatabaseSecurityInfo.Description;
+                SetPopupActions(DatabaseSecurityInfo.ActionsList);
                 break;
             case AreaTypes.RealtimeFraudAnalysis:
                 PopUpTitle.text = RealtimeFraudAnalysisInfo.Nom;
                 PopUpDescription.text = RealtimeFraudAnalysisInfo.Description;
+                SetPopupActions(RealtimeFraudAnalysisInfo.ActionsList);
                 break;
             case AreaTypes.HighSpeedTrading:
                 PopUpTitle.text = HighSpeedTradingInfo.Nom;
                 PopUpDescription.text = HighSpeedTradingInfo.Description;
+                SetPopupActions(HighSpeedTradingInfo.ActionsList);
                 break;
             case AreaTypes.AccountingDepartment:
                 PopUpTitle.text = AccountingDepartmentInfo.Nom;
                 PopUpDescription.text = AccountingDepartmentInfo.Description;
+                SetPopupActions(AccountingDepartmentInfo.ActionsList);
                 break;
             case AreaTypes.CryptoTrading:
                 PopUpTitle.text = CryptoTradingInfo.Nom;
                 PopUpDescription.text = CryptoTradingInfo.Description;
+                SetPopupActions(CryptoTradingInfo.ActionsList);
                 break;
         }
 
